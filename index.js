45b2e5c121781ad1938d9b60641bc537480635ec
daspete <daspetemail@gmail.com>
2020-01-03 00:10:46 +0100

    fix: testing

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ import bodyParser from 'body-parser'
 import compression from 'compression'
 
 export default (config) => {
-    return 'test'
-    // const app = express()
+    const app = express()
 
-    // app.use(helmet.hsts(config.helmet))
-    // app.use(cors(config.cors))
-    // app.use(compression())
+    app.use(helmet.hsts(config.helmet))
+    app.use(cors(config.cors))
+    app.use(compression())
 
-    // app.use(bodyParser.json(config.bodyParser.json))
-    // app.use(bodyParser.urlencoded(config.bodyParser.urlencoded))
+    app.use(bodyParser.json(config.bodyParser.json))
+    app.use(bodyParser.urlencoded(config.bodyParser.urlencoded))
 
-    // return app
+    return app
 }
