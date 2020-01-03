d9e49e46c859f77e267d10f9b16229ce6d82ade3
daspete <daspetemail@gmail.com>
2020-01-03 22:05:04 +0100

    refactor: added basic test

---
+++ test/index.js
@@ -0,0 +1,64 @@
+import ExpressService from '../'
+
+const serverConfig = {
+    // server ip and port
+    ip: process.env.SERVER_IP || '127.0.0.1',
+    port: process.env.SERVER_PORT || 3000,
+
+
+    // autorouting adds automatically routes recursively from the given directory
+    autoroute: 'routes', // set to false to disable autorouting
+
+
+    // security settings (configuration options, see: https://github.com/helmetjs/helmet)
+    helmet: {
+        disabled: false,
+        options: {
+            hsts: {
+                maxAge: 60 * 60 * 2,
+                includeSubDomains: false
+            }
+        }
+
+    },
+
+
+    // CORS settings (configuration options, see: https://github.com/expressjs/cors)
+    cors: {
+        disabled: false,
+        options: {
+            origin: '*',
+            optionsSuccessStatus: 200
+        }
+    },
+
+
+    // body parser settings (configuration options, see: https://github.com/expressjs/body-parser)
+    bodyParser: {
+        disabled: false,
+        options: {
+            json: {
+                limit: '64mb'
+            },
+            urlencoded: {
+                extended: true
+            }
+        }
+
+    },
+
+
+    // compression settings (configuration options, see: https://github.com/expressjs/compression)
+    compression: {
+        disabled: false,
+        options: {}
+    }
+}
+
+const server = new ExpressService(serverConfig)
+
+server.Use('/', async (req, res, next) => {
+    res.json('hello')
+})
+
+server.Start()
